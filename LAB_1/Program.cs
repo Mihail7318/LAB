dbe8b7bdd16e7f37bac15a6dab04842bf61dc543
Mihail7318 <50657490+Mihail7318@users.noreply.github.com>
2019-11-18 02:42:46 +0400

    Add files via upload

---
+++ LAB_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LAB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, b, V, S;
+            Console.WriteLine("Введите высоту h: ");
+            h = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Введите сторону b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            S = Math.Pow(b, 2);
+            V =  (S * h)/3;
+            Console.WriteLine("Объем пирамиды V: "+ Math.Round(V, 2));
+            Console.ReadKey();
+        }
+    }
+}
